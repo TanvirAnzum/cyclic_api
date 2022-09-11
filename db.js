1174ccf92ac79ed3d4b122d7c0b58317cad91031
unknown <tanviranzum14@gmail.com>
2022-09-11 20:18:56 +0600

    boards added

--- db.js
+++ db.js
@@ -2,11 +2,13 @@ const todos = require("./data/todos");
 const videos = require("./data/videos");
 const tags = require("./data/tags");
 const transactions = require("./data/transactions");
+const boards = require("./data/board.json");
 
 module.exports = {
   todos,
   videos,
   tags,
   transactions,
+  boards,
 }
 
